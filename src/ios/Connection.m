5b59f9416fe9e797d0c1dc83ea0421617d0b239c
ezengsvmx <edward.zeng@servicemax.com>
2017-07-07 09:55:23 -0700

    PFSA-869: TCP Socket Connection
    Fixed iOS build errors

--- src/ios/Connection.m
+++ src/ios/Connection.m
@@ -24,18 +24,18 @@
     // Init network communication settings
     CFReadStreamRef readStream;
     CFWriteStreamRef writeStream;
-    
+
     // Opening connection
     CFStreamCreatePairWithSocketToHost(NULL, (__bridge CFStringRef)_host, _port, &readStream, &writeStream);
-    
+
     // Configuring input stream
-    reader = objc_retainedObject(readStream);
+    reader = CFBridgingRelease(readStream);
     [reader setDelegate:self];
     [reader scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     [reader open];
-    
+
     // Configuring output stream
-    writer = objc_retainedObject(writeStream);
+    writer = CFBridgingRelease(writeStream);
     [writer setDelegate:self];
     [writer scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
     [writer open];
@@ -47,7 +47,7 @@
     [writer removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
     [writer setDelegate:nil];
     writer = nil;
-    
+
     // Closing input stream
     [reader close];
     [reader removeFromRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
@@ -66,22 +66,22 @@
 			NSLog(@"Stream opened!");
             connected = YES;
 			break;
-            
+
         // Data receiving
 		case NSStreamEventHasBytesAvailable:
             if (theStream == reader) {
                 uint8_t buffer[10240];
                 NSInteger len;
-                
+
                 while ([reader hasBytesAvailable]) {
                     len = [reader read : buffer maxLength : sizeof(buffer)];
-                    
+
                     if (len > 0) {
-                        
+
                         NSString *chunk = [[NSString alloc] initWithBytes : buffer
                                                                    length : len
                                                                  encoding : NSASCIIStringEncoding];
-                        
+
                         if (nil != chunk) {
                             NSLog(@"Received data: %@", chunk);
                             [_hook sendMessage : _host : _port : chunk];
@@ -90,17 +90,17 @@
                 }
             }
             break;
-            
+
         case NSStreamEventErrorOccurred:
             NSLog(@"Cannot connect to the host!");
             connected = NO;
             break;
-            
+
         case NSStreamEventEndEncountered:
             NSLog(@"Stream closed!");
             connected = NO;
             break;
-            
+
         default:
             NSLog(@"Unknown event!");
     }
